561538091e61208eb6a90e624f6d3a32cc4cc6cc
Tharunkumarbakka <tharunkumarbakka@gmail.com>
2022-07-01 23:29:00 +0530

    init

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1','sitesee.herokuapp.com/']
 # Application definition
 
 INSTALLED_APPS = [
+    #'whitenoise.runserver_nonstatic'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,8 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-MEDIA_ROOT = 'media'
+#MEDIA_URL = 'media/'
+#MEDIA_ROOT = "media"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'media')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
