c186c662a46549c3aac3e4788aafd79d2c38fc7c
Tharunkumarbakka <tharunkumarbakka@gmail.com>
2022-07-01 22:31:09 +0530

    init

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -3,9 +3,14 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('listings.urls'))
+    path('', include('listings.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 if settings.DEBUG:
